0326ba727be8fe552ee8466ebc70dfa87cd00115
Jerry <jerryshum2636@gmail.com>
2024-12-08 15:07:24 -0500

    styling login page

--- app/login/page.js
+++ app/login/page.js
@@ -1,8 +1,25 @@
-export default function Page() {
+export default function LoginPage() {
    return (
-      <div>
-         <h1>This is the login page.</h1>
+      <div className="h-full flex flex-col items-center justify-center">
+         <form className="max-w-sm mx-auto p-6 border border-zinc-800 rounded-xl">
+            <h2 className="text-lg font-bold mb-4">Login</h2>
+            <input
+               type="text"
+               placeholder="Username"
+               className="border px-4 py-2 mb-3 w-full rounded text-black"
+            />
+            <input
+               type="password"
+               placeholder="Password"
+               className="border px-4 py-2 mb-3 w-full rounded text-black"
+            />
+            <button
+               type="submit"
+               className="bg-foreground rounded text-black w-full text-center hover:bg-zinc-400 duration-300 py-2"
+            >
+               Log In
+            </button>
+         </form>
       </div>
    );
 }
-a;
