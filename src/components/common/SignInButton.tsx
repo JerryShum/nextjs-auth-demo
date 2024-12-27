859821905fc8a65230a9220703f68f0690bc3044
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2024-12-27 15:37:45 -0500

    Created SignInButton with signin action

---
+++ src/components/common/SignInButton.tsx
@@ -0,0 +1,14 @@
+import { signIn } from '@/actions/signIn';
+import { Button } from '../UI/button';
+import { ArrowBigRight } from 'lucide-react';
+
+export default function SignInButton() {
+   return (
+      <form action={signIn}>
+         <Button variant="outline" size="default">
+            <span className=" tracking-normal text-lg">Sign In</span>
+            <ArrowBigRight />
+         </Button>
+      </form>
+   );
+}
