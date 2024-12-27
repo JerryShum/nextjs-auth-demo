4ea5317c379813e0bd699850e5c5f65d6349d827
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2024-12-27 15:37:16 -0500

    setting up github authentication (provider)

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,16 @@
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
 
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+
+console.log(GITHUB_CLIENT_ID);
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-   providers: [GitHub({})],
+   providers: [
+      GitHub({
+         clientId: GITHUB_CLIENT_ID,
+         clientSecret: GITHUB_CLIENT_SECRET,
+      }),
+   ],
 });
