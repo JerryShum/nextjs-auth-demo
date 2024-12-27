75b49047af712171d83012a4c2ff8fceefb14a53
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2024-12-27 17:38:45 -0500

    NavAuth used in NavBar
    - Will display user avatar if there is a session, if not, it will display a signin button

--- src/components/nav/NavAuth.tsx
+++ src/components/nav/NavAuth.tsx
@@ -1,10 +1,23 @@
 'use client';
 import * as actions from '@/actions';
 import { useSession } from 'next-auth/react';
+import SignInButton from '../SignInButton';
+import AuthAvatar from './AuthAvatar';
 
 export default function NavAuth() {
    const session = useSession();
-   console.log(session);
 
-   return <div>NavAuth</div>;
+   let authContent: React.ReactNode;
+
+   //! If session is loading, don't show anything (prevents weird behaviour)
+   if (session.status === 'loading') {
+      authContent = null;
+   } else if (!session.data?.user || !session.data) {
+      //! If there is no session (user is currently not logged in)
+      authContent = <SignInButton />;
+   } else if (session.data?.user) {
+      //! If user is logged in
+      authContent = <AuthAvatar user={session.data.user} />;
+   }
+   return authContent;
 }
