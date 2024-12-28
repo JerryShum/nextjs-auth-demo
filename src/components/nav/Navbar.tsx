9496fb288be3669a07eeef54e5fbd28e91e4354a
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2024-12-28 16:10:08 -0500

    adjusting navbar for signup route

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -24,9 +24,9 @@ export default function Navbar() {
                      <span className="text-md">Login Page</span>
                   </Button>
                </Link>
-               <Link href={'/'}>
+               <Link href={'/signup'}>
                   <Button variant={'link'} size={'default'}>
-                     <span className="text-md">Home</span>
+                     <span className="text-md">Sign Up</span>
                   </Button>
                </Link>
 
