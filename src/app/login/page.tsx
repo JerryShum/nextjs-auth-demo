6b3c16ac8cb6efa684faca4bb75d8c50e6d87cdb
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2024-12-28 15:24:12 -0500

    styling login page/form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,11 @@ import LoginForm from '@/components/login/LoginForm';
 export default function LoginPage() {
    return (
       <div className="flex flex-col items-center">
-         <h1>this is hte login page.</h1>
+         <h1 className="font-sans text-5xl font-bold mt-20 mb-10 text-center">
+            <span className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent tracking-tight">
+               Auth.js Custom Login Page
+            </span>{' '}
+         </h1>
          <LoginForm />
       </div>
    );
--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,20 +1,64 @@
+import { Cat, DogIcon, GithubIcon } from 'lucide-react';
+import { Button } from '../UI/button';
+import { Input } from '../UI/input';
+import { Label } from '../UI/label';
+import { Separator } from '../UI/separator';
+
 export default function LoginForm() {
    return (
-      <div className="p-2 border border-muted rounded-lg flex flex-col items-center max-w-96">
-         <h2>Login</h2>
-         <form>
-            <div>
-               <label htmlFor="email">Email:</label>
-               <input type="email" id="email" name="email" required />
+      <div className="p-8 border-2 border-muted rounded-lg flex flex-col items-center max-w-96 h-full">
+         <h2 className="text-4xl font-semibold">Login</h2>
+         <div className="flex flex-col mt-4 min-h-full min-w-64">
+            <form>
+               <div className="flex flex-col gap-4">
+                  <div>
+                     <Label htmlFor="email" className="text-lg tracking-wider">
+                        Email
+                     </Label>
+                     <Input
+                        type="email"
+                        id="email"
+                        placeholder="Email"
+                        name="email"
+                     />
+                  </div>
+                  <div>
+                     <Label
+                        htmlFor="password"
+                        className="text-lg tracking-wider"
+                     >
+                        Password
+                     </Label>
+                     <Input
+                        type="password"
+                        id="password"
+                        placeholder="Password"
+                        name="password"
+                     />
+                  </div>
+                  <Button className="" size={'lg'}>
+                     <span className="font-semibold">Login</span>
+                  </Button>
+               </div>
+            </form>
+
+            <h4 className="text-center text-muted-foreground my-4">or</h4>
+
+            <div className="flex flex-col gap-4">
+               <Button size={'lg'}>
+                  <span className="font-semibold tracking-wide">
+                     Continue With GitHub
+                  </span>{' '}
+                  <Cat className="!w-8 !h-8" />
+               </Button>
+               <Button size={'lg'}>
+                  <span className="font-semibold tracking-wide">
+                     Continue With Google
+                  </span>{' '}
+                  <DogIcon className="!w-8 !h-8" />{' '}
+               </Button>
             </div>
-            <div>
-               <label htmlFor="password">Password:</label>
-               <input type="password" id="password" name="password" required />
-            </div>
-            <button type="submit">Login</button>
-         </form>
-         <button>Login with GitHub</button>
-         <button>Login with Google</button>
+         </div>
       </div>
    );
 }
