56e900cc356ba9c6cb50f47df044839bf611e8f2
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2024-12-28 16:09:47 -0500

    renaming login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,7 +5,7 @@ export default function LoginPage() {
       <div className="flex flex-col items-center">
          <h1 className="font-sans text-5xl font-bold mt-20 mb-10 text-center">
             <span className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent tracking-tight">
-               Auth.js Custom Login Page
+               Login Page
             </span>{' '}
          </h1>
          <LoginForm />
