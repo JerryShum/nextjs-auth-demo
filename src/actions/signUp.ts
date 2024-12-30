3dc90599b26abdf8acb5887658e833d82952ea23
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2024-12-29 23:22:11 -0500

    punctuation fixing errors

--- src/actions/signUp.ts
+++ src/actions/signUp.ts
@@ -7,15 +7,15 @@ const signUpFormSchema = z
       email: z.string().email().min(3).nonempty('Email is required.'),
       password: z
          .string()
-         .min(8, 'Password must be at least 8 characters long')
+         .min(8, 'Password must be at least 8 characters long.')
          .regex(
             /^(?=.*[!@#$%^&*])(?=.*[A-Z])/,
-            'Password must include an uppercase letter, a lowercase letter, a digit, and a special character'
+            'Password must include an uppercase letter, a lowercase letter, a digit, and a special character.'
          ),
       confirmPassword: z.string(),
    })
    .refine((data) => data.password === data.confirmPassword, {
-      message: 'Passwords must match',
+      message: 'Passwords must match.',
       path: ['confirmPassword'],
    });
 
